2c2b4c6ec8c14c8f6525fff49dfcd7d397321195
Suman Mandal <sumanmandal15135@gmail.com>
2020-12-13 00:49:45 +0530

    Switch Case statement

---
+++ 8_switchCase.dart
@@ -0,0 +1,24 @@
+
+
+void main() {
+  //switch case statement: only applicable to "string" and "int"
+  String grade = "A";
+
+  switch (grade) {
+    case "A" :
+      print("U got grade A great !");
+      break;
+
+    case "B":
+      print("U got B work hard");
+      break;
+
+    case "C":
+      print("C mila he be BSDK");
+      break;
+
+    case "D":
+      print("Tum se na ho payega");
+      break;
+  }
+}
